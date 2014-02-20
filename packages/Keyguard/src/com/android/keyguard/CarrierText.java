389b775e874a1b282adb0077c874c6b3fdcabea3
metalspring <cgibson16137@gmail.com>
2014-02-20 17:44:53 -0500

    Revert "[1/2] Base: Custom carrier label"
    This reverts commit 5440f5472b1548d7b181680973e831c250961391.

--- packages/Keyguard/src/com/android/keyguard/CarrierText.java
+++ packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -17,7 +17,6 @@
 package com.android.keyguard;
 
 import android.content.Context;
-import android.provider.Settings;
 import android.text.method.SingleLineTransformationMethod;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -87,13 +86,7 @@ public class CarrierText extends TextView {
     }
 
     protected void updateCarrierText(State simState, CharSequence plmn, CharSequence spn) {
-        String customLabel = Settings.Global.getString(mContext.getContentResolver(),
-                Settings.Global.NOTIFICATION_CUSTOM_CARRIER_LABEL);
-        if (customLabel != null && customLabel.length() > 0) {
-            setText(customLabel);
-        } else {
-            setText(getCarrierTextForSimState(simState, plmn, spn));
-        }
+        setText(getCarrierTextForSimState(simState, plmn, spn));
     }
 
     @Override
